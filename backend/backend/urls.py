2f602c6f5cd2f9d4f2629b32b939fc627510626c
Taiwo <ajayitaiwotunta@gmail.com>
2023-03-19 03:49:12 +0100

    updateurl

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,7 +25,8 @@ urlpatterns = [
     path('api/', include('base.urls')),
     path('', views.index),
 
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) \
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
